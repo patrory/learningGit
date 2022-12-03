88878a856cd4b7a2c2da79e6989d64ed189126a6
pratik <21pratikdhane@gmail.com>
2022-12-03 13:25:12 +0530

    B created

---
+++ b.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a = 10;
+    for (int i = 0; i < 10; i++)
+    {
+        a += i;
+    }
+    cout << a << endl;
+    return 0;
+}
